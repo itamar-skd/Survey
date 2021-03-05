f291f6929a31e7a1cd9cf8ccff815b32a35eb837
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-03-05 22:33:26 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 
 module.exports = client => {
     console.log(`>>> Active as ${client.user.username}`)
+    client.user.setPresence({ activity: {name: `${client.guilds.cache.size} Servers!`, type: 'WATCHING'}, status: 'dnd'})
+    setInterval(() => {
+        client.user.setPresence({ activity: {name: `${client.guilds.cache.size} Servers!`, type: 'WATCHING'}, status: 'dnd'})
+    }, 1000 * 60 * 10)
 
     mongoose.connect(process.env.MONGOOSE, {
         useUnifiedTopology: true,
